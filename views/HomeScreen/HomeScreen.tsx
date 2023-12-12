03e8275716d4e835637c029b7d68d40ba8a43c90
MarcosPinilla <m.pinilla03@ufromail.cl>
2023-12-12 17:55:31 -0300

    show basic information of films in HomeScreen

--- views/HomeScreen/HomeScreen.tsx
+++ views/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,46 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { View , Text} from 'react-native'
+import { connect } from 'react-redux';
+import { fetchFilmsRequest } from '../../redux/actions/';
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>Hola mundo!</Text>
-    </View>
-  )
+interface HomeScreenProps {
+  films: any[];
+  loadingFilms: boolean;
+  fetchFilmsRequest: () => void;
 }
 
-export{ HomeScreen }
+const HomeScreen: React.FC<HomeScreenProps> = ({ films, loadingFilms, fetchFilmsRequest }) => {
+  useEffect(() => {
+    fetchFilmsRequest();
+  }, [fetchFilmsRequest]);
+
+  console.log(loadingFilms);
+  
+  if(loadingFilms){
+    return (
+        <View>
+            <Text>Cargando...</Text>
+        </View>
+    )
+  }
+  return (
+    <View>
+      {/* Renderiza la información de las películas */}
+      {films.map((film) => (
+        <Text key={film.title}>{film.title}</Text>
+      ))}
+    </View>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  films: state.films.films,
+  loadingFilms: state.films.loading
+});
+
+const mapDispatchToProps = {
+  fetchFilmsRequest,
+};
+
+const connectHomeScreen = connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+export { connectHomeScreen as HomeScreen };
