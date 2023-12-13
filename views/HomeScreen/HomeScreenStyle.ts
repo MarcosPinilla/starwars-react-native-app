000b339ef89a99da1e9bf4453e05c993c1b4f6f6
MarcosPinilla <m.pinilla03@ufromail.cl>
2023-12-13 16:09:28 -0300

    add FilmCard component and imdb movie api for get image film

---
+++ views/HomeScreen/HomeScreenStyle.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#F9F9F9",
+    height: "100%",
+  },
+  loadingText: {
+    alignSelf: "center",
+  },
+});
+
+export default { s };
